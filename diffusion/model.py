3090f57b403727c4f090970c0031fd275cdbcf4c
enamespace <enamespace@gmail.com>
2023-08-15 23:29:42 +0800

    before sampling

--- diffusion/model.py
+++ diffusion/model.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from einops import rearrange, reduce
 from einops.layers.torch import Rearrange
 
+
 def exists(x):
     return x is not None
 
@@ -19,7 +20,7 @@ def default(val, d):
 
 
 def num_to_groups(num, divisor):
-    groups  = num // divisor 
+    groups = num // divisor
     remainder = num % divisor
     arr = [divisor] * groups
     if remainder > 0:
@@ -34,17 +35,21 @@ class Residual(nn.Module):
 
     def forward(self, x, *args, **kwargs):
         return self.fn(x, *args, **kwargs) + x
-    
+
+
 def Upsample(dim, dim_out=None):
     return nn.Sequential(
         nn.Upsample(scale_factor=2, mode="nearset"),
         nn.Conv2d(dim, default(dim_out, dim), 3, padding=1)
     )
+
+
 def Downsample(dim, dim_out=None):
     return nn.Sequential(
         Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
         nn.Conv2d(dim*4, default(dim_out, dim), 1)
     )
+
 
 class SinPositionEmbeddings(nn.Module):
     def __init__(self, dim):
@@ -54,8 +59,456 @@ class SinPositionEmbeddings(nn.Module):
     def forward(self, time):
         device = time.device
         half_dim = self.dim // 2
-        embediings = math.log(100000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embediings)
+        embeddings = math.log(100000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(
+            half_dim, device=device) * -embeddings)
         embeddings = time[:, None] * embeddings[None, :]
         embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
         return embeddings
+
+
+class WeightStandardizedConv2d(nn.Conv2d):
+    def forward(self, x):
+        eps = 1e-5 if x.type == torch.float32 else 1e-3
+
+        weight = self.weight
+        mean = reduce(weight, "o ... -> o 1 1 1", "mean")
+        var = reduce(weight, "o ... -> o 1 1 1",
+                     partial(torch.var, unbiased=False))
+
+        normalized_weight = (weight - mean) * (var + eps).rsqrt()
+
+        return F.conv2d(
+            x,
+            normalized_weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups
+        )
+
+
+class Block(nn.Module):
+    def __init__(self, dim, dim_out, groups=8) -> None:
+        super().__init__()
+        self.proj = WeightStandardizedConv2d(dim, dim_out, 3, padding=1)
+        self.norm = nn.GroupNorm(groups, dim_out)
+        self.act = nn.SiLU()
+
+    def forward(self, x, scale_shift=None):
+        x = self.proj(x)
+        x = self.norm(x)
+
+        if exists(scale_shift):
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+
+        x = self.act(x)
+        return x
+
+
+class ResnetBlock(nn.Module):
+
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, groups=8) -> None:
+        super().__init__()
+        self.mlp = (
+            nn.Sequential(nn.SiLU(), nn.Linear(time_emb_dim, dim_out * 2))
+            if exists(time_emb_dim)
+            else None
+        )
+
+        self.block1 = Block(dim, dim_out, groups=groups)
+        self.block2 = Block(dim, dim_out, groups=groups)
+        self.res_conv = nn.Conv2d(
+            dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, time_emb=None):
+        scale_shift = None
+        if exists(self.mlp) and exists(time_emb):
+            time_emb = self.mlp(time_emb)
+            time_emb = rearrange(time_emb, "b c -> b c 1 1")
+            scale_shift = time_emb.chunk(2, dim=1)
+
+        h = self.block1(x, scale_shift=scale_shift)
+        h = self.block2(h)
+
+        return h + self.res_conv(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32) -> None:
+        super().__init__()
+
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
+
+    # channel 相当于dimension
+    def forward(self, x):
+        b, c, h, w = x.shape
+
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+
+        q, k, v = map(
+            lambda t: rearrange(
+                t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+
+        q = q * self.scale
+
+        sim = einsum("b h d i, b h d j -> b h i j", q, k)
+
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        attn = sim.softmax(dim=-1)
+
+        out = einsum("b h i j, b h d j -> b h i d", attn, v)
+        out = rearrange(out, "b h (x y) d -> b (h d) x y", x=h, y=w)
+        return self.to_out(out)
+
+
+class LinearAttention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32) -> None:
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+        self.to_out = nn.Sequential(
+            nn.Conv2d(hidden_dim, dim, 1), nn.GroupNorm(1, dim))
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads)
+
+        )
+
+        # 保证q,k 非负则内积非负 https://zhuanlan.zhihu.com/p/157490738
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
+
+        q = q * self.scale
+
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
+
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+
+        out = rearrange(out, "b h c (x y) -> b (h c) x y",
+                        h=self.heads, x=h, y=w)
+
+        return self.to_out(out)
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn) -> None:
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.GroupNorm(1, dim)
+
+    def forward(self, x):
+        x = self.norm(x)
+        return self.fn(x)
+
+
+class Unet(nn.Module):
+    def __init__(self,
+                 dim,
+                 init_dim,
+                 out_dim,
+                 dim_mults=(1, 2, 4, 8),
+                 channels=3,
+                 self_condition=False,  # 什麼是self_condition
+                 resnet_block_groups=4
+                 ):
+        super().__init__()
+
+        self.channels = channels
+
+        self.self_condition = self_condition
+        input_channels = channels * (2 if self_condition else 1)
+
+        init_dim = default(init_dim, dim)
+
+        # 维度为channel数
+        self.init_conv = nn.Conv2d(input_channels, init_dim, 1, padding=0)
+
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        block_klass = partial(ResnetBlock, groups=num_to_groups)
+
+        time_dim = dim * 4
+
+        self.time_mlp = nn.Sequential(
+            SinPositionEmbeddings(dim),
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim)
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+            self.downs.append(
+                nn.ModuleList(
+                    [
+
+                        # 兩個block_klass有必要嗎，一個不可以嗎
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                        Downsample(dim_in, dim_out)
+                        if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1)
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = (ind == len(in_out) - 1)
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        # 為什麼這裡要加之前的兩個block的輸出
+                        block_klass(dim_out + dim_in, dim_out,
+                                    time_emb_dim=time_dim),
+                        block_klass(dim_out + dim_in, dim_out,
+                                    time_emb_dim=time_dim),
+                        Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1)
+                    ]
+                )
+            )
+
+        self.out_dim = default(out_dim, channels)
+
+        self.final_res_block = block_klass(dim * 2, dim, time_emb_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time, x_self_cond=False):
+        if self.self_condition:
+            x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
+            x = torch.cat((x_self_cond, x), dim=1)
+
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+
+            # 為什麼這裡沒有h.append
+            x = block2(x, t)
+
+            x = attn(x)
+
+            h.append(x)
+
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x, t)
+        x = self.mid_block2(x, t)
+
+        for block1, block2, attn, upsample, in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+
+            x = attn(x)
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+
+        x = self.final_res_block(x, t)
+
+        return self.final_conv(x)
+
+
+
+
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    """
+
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps)
+    alphas_cumprod = torch.cos(((x/timesteps) + s) / (1+s) * torch.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+
+    betas = 1 - (alphas_cumprod[1:]/ alphas_cumprod[:-1])
+
+    return torch.clip(betas, 0.0001, 0.9999)
+
+
+def linear_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start, beta_end, timesteps)
+
+def quadratic_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start**0.5, beta_end**0.5, timesteps) ** 2
+
+
+def sigmoid_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    betas = torch.linspace(-6, 6, timesteps)
+    return torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
+
+
+timesteps = 300
+
+betas = linear_beta_schedule(timesteps=timesteps)
+
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+# q(x_t|x_{t-1})
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+
+# q(x_{t-1}|x_t, x_0)
+
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1 - alphas_cumprod)
+
+# 存疑
+def extract(a, t, x_shape):
+    batch_size = t.shape[0]
+    out = a.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1, ) * (len(x_shape)-1))).to(t.device)
+
+from PIL import Image
+import requests
+
+# url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+# image = Image.open(requests.get(url, stream=True).raw) # PIL image of shape HWC
+# image
+
+from torchvision.transforms import Compose, ToTensor, Lambda, ToPILImage, CenterCrop, Resize
+
+image_size = 128
+
+transform = Compose([
+    Resize(image_size),
+    CenterCrop(image_size),
+    ToTensor(),
+    Lambda(lambda t: (t*2)-1) # [-1, 1]
+])
+
+# x_start = transform(image).unsqueeze(0)
+# x_start.shape
+
+
+
+import numpy as np
+
+reverse_transform = Compose([
+    Lambda(lambda t: (t+1)/2),
+    Lambda(lambda t: t.permute(1,2,0)),
+    Lambda(lambda t: t * 255.),
+    Lambda(lambda t: t.numpy().astype(np.uint8)),
+    ToPILImage()
+])
+
+# reverse_transform(x_start.squeeze())
+
+
+def q_sample(x_start, t, noise=None):
+    noise = default(noise, torch.rand_like(x_start))
+    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(sqrt_one_minus_alphas_cumprod, t, x_start.shape)
+
+    return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+def get_noisy_image(x_start, t):
+  # add noise
+  x_noisy = q_sample(x_start, t=t)
+
+  # turn back into PIL image
+  noisy_image = reverse_transform(x_noisy.squeeze())
+
+  return noisy_image
+
+# # take time step
+# t = torch.tensor([40])
+
+# get_noisy_image(x_start, t)
+
+def p_losses(denoise_model, x_start, t, noise=None, loss_type="l1"):
+    if noise is None:
+        noise = torch.rand_like(x_start)
+    
+    x_noisy = q_sample(x_start=x_start, t=t, noise=noise)
+    predicted_noise = denoise_model(x_noisy, t)
+
+    if loss_type == 'l1':
+        loss = F.l1_loss(noise, predicted_noise)
+    elif loss_type == 'l2':
+        loss = F.mse_loss(noise, predicted_noise)
+    elif loss_type == 'huber':
+        loss = F.smooth_l1_loss(noise, predicted_noise)
+    else:
+        raise NotImplementedError()
+    
+    return loss
+
+
+from datasets import load_dataset
+
+dataset = load_dataset('fashion_mnist')
+image_size = 28
+channels = 1
+batch_size = 128
+
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+transform = Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda t: (t * 2) - 1)
+])
+
+
+def transforms(examples):
+    examples['pixel_values'] = [transform(image.convert('L')) for image in examples['image']]
+    del examples["images"]
+
+    return examples
+
+transformed_dateset = dataset.with_transform(transforms).remove_columns("label")
+
+dataloader = DataLoader(transformed_dateset['train'], batch_size=batch_size, shuffle=True)
+
+# Sampling
